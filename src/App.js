e2f0c30e8115684b6a1a8d9b49579e178cf5688e
choikyubeom <choigane_kb@naver.com>
2022-03-08 07:32:26 +0900

    apply material ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import { Component } from 'react';
 import Customer from './Com/Customer';
 
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 const Cus = [{
   'id': 1,
   'test1': 'https://placeimg.com/64/64/1',
@@ -20,20 +26,34 @@ class App extends Component {
 
   render() {
     return (
-    <div>
-      {
-        Cus.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}              
-              test1={c.test1}
-              test2={c.test2}
-            />
-          );
-        })
-      }
-    </div>
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>1</TableCell>
+              <TableCell>2</TableCell>
+              <TableCell>3</TableCell>
+              <TableCell>4</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody> 
+            {
+              Cus.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    test1={c.test1}
+                    test2={c.test2}
+                  />
+                );
+              })
+
+            }
+          </TableBody>
+        </Table>
+
+      </div>
     )
   }
 }
--- src/Com/Customer.js
+++ src/Com/Customer.js
@@ -1,47 +1,48 @@
 import React from 'react';
 
-class Customer extends React.Component
-{
-    render()
-    {
-        return(
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+class Customer extends React.Component {
+    render() {
+        return (
             <div>
-           <Customer1 id={this.props.id}></Customer1>
-           <Customer2 test1={this.props.test1}
-           test2={this.props.test2}
-           ></Customer2>
-        </div>
+                <TableRow>
+                    <TableCell>{this.props.id}</TableCell>
+                    <TableCell>{this.props.test1}</TableCell>
+                    <TableCell>   <img src={this.props.test1} alt="image" /></TableCell>
+                   
+                    <TableCell>{this.props.test2}</TableCell>
+                </TableRow>
+                <Customer1 id={this.props.id}></Customer1><Customer2 test1={this.props.test1} test2={this.props.test2}           ></Customer2>
+            </div>
         )
     }
 
 }
 
-class Customer1 extends React.Component
-{
-    render()
-    {
-        return(
+class Customer1 extends React.Component {
+    render() {
+        return (
             <div>
-            <h2>{this.props.id}</h2> 
-        </div>
+                <h2>{this.props.id}</h2>
+            </div>
         )
     }
 
 }
 
-class Customer2 extends React.Component
-{
-    render()
-    {
-        return(
+class Customer2 extends React.Component {
+    render() {
+        return (
             <div>
-            <h2>{this.props.test1}</h2>
-            <img src={this.props.test1} alt="image"/>
-            <p>{this.props.test2}</p>
-        </div>
+                <h2>{this.props.test1}</h2>
+                <img src={this.props.test1} alt="image" />
+                <p>{this.props.test2}</p>
+            </div>
         )
     }
 
 }
 
-export  default Customer;
+export default Customer;
